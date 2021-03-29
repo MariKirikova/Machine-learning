bf34cbb534a39963fee24fe32c467b82a091c0bb
MariKirikova <kirikovamasha@gmail.com>
2021-03-29 15:17:23 +0300

    сщььше

--- Machine-learning/main.py
+++
@@ -1,6 +0,0 @@
-from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
-from kivy.animation import Animation
-#Ой все таки не лишний
