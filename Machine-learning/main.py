add406e07fe24e7fca141875cef91a63d7a1f213
MariKirikova <kirikovamasha@gmail.com>
2021-03-29 15:06:44 +0300

    правка и новый файл

--- Machine-learning/main.py
+++ Machine-learning/main.py
@@ -1,2 +1,6 @@
 from kivy.app import App
-#Лишний код
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.animation import Animation
+#Ой все таки не лишний
